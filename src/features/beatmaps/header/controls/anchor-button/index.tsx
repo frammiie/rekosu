c3dee82dabd9337692b84b831e7ea6f775b5a859
frammie <fraym.phone@gmail.com>
2025-06-01 22:54:17 +0200

    Fix osu!direct anchor target

--- src/features/beatmaps/header/controls/anchor-button/index.tsx
+++ src/features/beatmaps/header/controls/anchor-button/index.tsx
@@ -1,12 +1,12 @@
 import { A, type AnchorProps } from '@solidjs/router';
 import type { ParentProps } from 'solid-js';
 
-export function AnchorButton(props: ParentProps & Pick<AnchorProps, 'href'>) {
+export function AnchorButton(props: ParentProps & AnchorProps) {
   return (
     <A
-      href={props.href}
       target='_blank'
       class='h-full p-[10px] bg-[#0005] hover:bg-[#0007] transition-colors text-xs font-medium flex items-center justify-center rounded-[10px] backdrop-blur-sm'
+      {...props}
     >
       <div class='flex items-center gap-[20px]'>{props.children}</div>
     </A>
--- src/features/beatmaps/header/controls/index.tsx
+++ src/features/beatmaps/header/controls/index.tsx
@@ -37,7 +37,7 @@ export function Controls(props: ControlsProps) {
             />
           </svg>
         </AnchorButton>
-        <AnchorButton href={`osu://b/${props.beatmap.id}`}>
+        <AnchorButton href={`osu://b/${props.beatmap.id}`} target='_self'>
           <span>osu!direct</span>
           <svg
             xmlns='http://www.w3.org/2000/svg'

732674a2eabc11e8d2ded868ac81514b296896cd
frammie <fraym.phone@gmail.com>
2025-06-02 23:44:10 +0200

    Remove superfluous code

--- src/features/beatmaps/search/index.tsx
+++ src/features/beatmaps/search/index.tsx
@@ -1,8 +1,6 @@
 import { useNavigate } from '@solidjs/router';
 import { createSignal } from 'solid-js';
 
-export type SearchProps = {};
-
 export function Search() {
   const [id, setId] = createSignal('');
   const navigate = useNavigate();
@@ -15,9 +13,7 @@ export function Search() {
         class='p-[5px] px-[10px] rounded-full bg-[#fff1] focus:outline-none focus:outline-[#fff6] focus:-outline-offset-2 placeholder:text-[#fff6]'
         value={id()}
         onChange={e => setId(e.currentTarget.value)}
-      >
-        asfd
-      </input>
+      />
       <button
         class='p-[5px] px-[20px] rounded-full bg-[#fff1] focus:outline-none focus:outline-[#fff6] focus:-outline-offset-2'
         onClick={() => navigate(`/beatmaps/${id()}`)}

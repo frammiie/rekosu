17f8520fa3918955d4f6cba06e7a903c8d4f353f
frammie <fraym.phone@gmail.com>
2025-06-04 21:40:09 +0200

    Add analytics scripts

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -7,6 +7,12 @@ export default createHandler(() => (
         <head>
           <meta charset='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <script
+            defer
+            src='https://an.rekosu.sh/script.js'
+            data-website-id='d69f7a9c-76d0-4c01-a318-5036a71f9f71'
+            data-host-url='https://osu.rekosu.sh'
+          />
           {assets}
         </head>
         <body>
